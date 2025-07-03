4d5aca118ad1de91afe80953c50bf91427c6be28
thansoe-2000 <tt6289216@gmail.com>
2025-07-03 20:32:00 +0630

    instance attribute

---
+++ PyExceptions/attributeError.py
@@ -0,0 +1,7 @@
+# attribute error
+
+name = "Than Soe"
+name.push("Hello")
+
+# AttributeError: 'str' object has no attribute 'push'
+# String မှာ push() ဆိုတဲ့ method မရှိဘူး။
---
+++ PyExceptions/instanceAttribute.py
@@ -0,0 +1,18 @@
+# instance attribute
+class Person:
+    def __init__(self, name, age):
+        self.name = name # instance attribute
+        self.age = age # instance attribute
+
+p1 = Person("Than Soe", 24)
+p2 = Person("Aung Aung", 22)
+
+print(p1.name)
+print(p2.name)
+print(type(p1))
+
+# p1 and p2 are objects,
+# they are created as object using Person() class
+# Person is class
+# p1 and p2 ထဲမှာ name, age ဆိုရေ Attribute တိ ပါရေ
+# object တစ်ခုချင်းစီမှာ မတူရေ data တိကို store လုပ် ဖို့ သုံးရေ။
